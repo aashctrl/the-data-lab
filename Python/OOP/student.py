70e462e1cf3b79e176593c21e4707b443973ff46
Aashi <aashi114312@gmail.com>
2026-06-27 21:57:41 +0530

    feat: add Student class

---
+++ Python/OOP/student.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, grade, subject):
+        self.name = name
+        self.grade = grade
+        self.subject = subject
+    def __str__(self):
+        return f"Name: {self.name}, Grade: {self.grade}, Subject: {self.subject}"
+
+s1 = Student("Aditya", "A", "Maths")
+s2 = Student("Sanvi", "B", "Science")
+print(s1)
+print(s2.grade)

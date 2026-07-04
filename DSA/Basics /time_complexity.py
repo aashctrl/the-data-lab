2a104a219c859318a4397e07754594230815a4cd
Aashi <aashi114312@gmail.com>
2026-07-04 21:34:25 +0530

    feat: add DSA time complexity examples

---
+++ DSA/Basics /time_complexity.py
@@ -0,0 +1,25 @@
+# DSA- Time Complexity Examples
+
+# O(1)
+print("Hello World")
+
+# O(n)
+n = 5
+for i in range(n):
+    print(i)
+
+# O(n²)
+for i in range(n):
+    for j in range(n):
+        print(i, j)
+
+# O(1)
+arr = [10, 20, 30, 40, 50, 60]
+print(arr[5])
+
+
+# O(1)
+a = 15
+b = 25
+x = a + b
+print(x)

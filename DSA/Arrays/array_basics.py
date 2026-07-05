edfb709524415a2874cf9a558ff676540caaa48c
Aashi <aashi114312@gmail.com>
2026-07-05 21:39:53 +0530

    feat: add array basics implementation

---
+++ DSA/Arrays/array_basics.py
@@ -0,0 +1,18 @@
+arr = [10, 20, 30, 40, 50]  
+
+# Accessing elements 
+print("First Element:", arr[0]) 
+print("Middle Element:", arr[2]) 
+print("Last Element:", arr[4])  
+
+# Updating 
+arr[2] = 300 
+print("Updated Array:", arr)  
+
+# Traversal 
+print("\nTraversing Array:") 
+for item in arr:     
+  print(item) 
+
+# Length 
+print("\nLength:", len(arr))
